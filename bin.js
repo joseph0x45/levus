79bc773d07eae0d25d4a6ea8e122c68eac878fa3
TheWisePigeon <zozozozeph@gmail.com>
2023-06-08 22:20:38 +0000

    updates

--- bin.js
+++ bin.js
@@ -1,4 +1,38 @@
 #!/usr/bin/env node
 
-import { svelte } from "./utils";
+import svelte from "./svelte.js";
+import { log } from "console"
+import { generateComponentTree } from "./utils.js";
+
+const frameworks = {
+    svelte
+}
+
+const args = process.argv
+
+if (args[2] === "help") {
+    log("Help this nigga")
+}
+
 //npx levus ls
+if (args[2] === "ls") {
+    /**
+        * @type { "svelte" }
+    */
+    const framework = args[3] ?? ""
+    if (framework === "") {
+        log("Specify a framework. Available ones are svelte")
+        process.exit(1)
+    }
+    if(!frameworks[framework] ) {
+        log("Invalid framework")
+        process.exit(1)
+    }
+    generateComponentTree(frameworks[framework])
+
+}
+
+//npx levus add
+if (args[2] === "add") {
+
+}
--- utils.js
+++ utils.js
@@ -1,10 +1,27 @@
-export const svelte = {
-    path:"./components/svelte",
-    layouts : [
-        {
-            name:"Centered",
-            description:"A res"
-        }
-    ],
-    ui: []
+import chalk from "chalk";
+
+export function generateComponentTree(component, indent = 0) {
+  const indentString = '  '.repeat(indent);
+
+  console.log(indentString + chalk.bold(component.name));
+
+  if (component.layouts && component.layouts.length > 0) {
+    console.log(indentString + '  - ' + chalk.bold('Layouts'));
+    component.layouts.forEach((layout) => {
+      console.log(indentString + '    - ' + chalk.green(layout.name) + ': ' + layout.description);
+    });
+  }
+
+  if (component.ui && component.ui.length > 0) {
+    console.log(indentString + '  - ' + chalk.bold('UI'));
+    component.ui.forEach((uiComponent) => {
+      console.log(indentString + '    - ' + chalk.blue(uiComponent.name) + ': ' + uiComponent.description);
+    });
+  }
+
+  if (component.subcomponents && component.subcomponents.length > 0) {
+    component.subcomponents.forEach((subcomponent) => {
+      generateComponentTree(subcomponent, indent + 1);
+    });
+  }
 }
