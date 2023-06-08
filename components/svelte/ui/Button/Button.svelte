66158a2519fef9140e6620a03d86675cdf81d040
TheWisePigeon <zozozozeph@gmail.com>
2023-06-08 21:36:49 +0000

    add Button ui component

---
+++ components/svelte/ui/Button/Button.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    export let title: string
+</script>
+
+<button
+    class="mt-4 h-10 md:h-12 md:text-lg w-full rounded-md border border-sky-600 bg-sky-600 font-semibold transition-all hover:font-bold dark:text-white dark:hover:bg-neutral-950 dark:hover:text-sky-600 duration-500"
+    >{title}</button
+>
---
+++ utils.js
@@ -0,0 +1,10 @@
+export const svelte = {
+    path:"./components/svelte",
+    layouts : [
+        {
+            name:"Centered",
+            description:"A res"
+        }
+    ],
+    ui: []
+}
