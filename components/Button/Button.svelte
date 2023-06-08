fc6324f081a43f0b6321fb768e2546dba016c845
TheWisePigeon <zozozozeph@gmail.com>
2023-06-08 19:14:24 +0000

    add centered div and button

---
+++ components/Button/Button.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let title: string
+</script>
+
+<button
+    class="mt-4 h-10 md:h-12 md:text-lg w-full rounded-md border border-sky-600 bg-sky-600 font-semibold transition-all hover:font-bold text-white hover:bg-neutral-950 hover:text-sky-600 duration-500"
+    >{title}</button
+>
+
