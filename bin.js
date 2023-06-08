72753f372062d6157fe4b775fe8efa3ef16b3cb4
TheWisePigeon <zozozozeph@gmail.com>
2023-06-08 23:52:50 +0000

    updates

--- bin.js
+++ bin.js
@@ -2,11 +2,19 @@
 
 import svelte from "./svelte.js";
 import { log } from "console"
-import { generateComponentTree } from "./utils.js";
+import { generateComponentTree, framework_exists } from "./utils.js";
 import select from "@inquirer/select";
+import fs from "fs-extra"
+import inquirer from "inquirer";
+const { existsSync } = fs
 
 const frameworks = {
     svelte
+}
+
+const RECOGNIZABLE_FILES = {
+    svelte: "svelte.config.js",
+    nextjs: "next.config.js"
 }
 
 const args = process.argv
@@ -14,6 +22,28 @@ const args = process.argv
 if (args[2] === "help") {
     log("Help this nigga")
 }
+
+if (args[2] === "init") {
+    log("Welcome to Levus CLI.")
+    log("Take a few seconds to set up Levus in your app")
+    let detected_framework = ""
+    for (const key in RECOGNIZABLE_FILES) {
+        if(existsSync(RECOGNIZABLE_FILES[key])){
+            detected_framework = key
+            break
+        }
+    }
+    if(detected_framework!==""){
+        log(`We detected that you are in a ${detected_framework} project`)
+        log("Where do you want Levus to import your components?")
+        const { location } = await inquirer.prompt({ name:"location", default:"./src/lib/ui/levus" })
+        log(location)
+        process.exit(1)
+    }
+    log("No framework detected. Tell us what framework are you using")
+    process.exit(1)
+}
+
 
 if (args[2] === "ls") {
     /**
@@ -37,7 +67,17 @@ if (args[2] === "ls") {
 
 }
 
-//npx levus add
+//npx asda add ui/Button
 if (args[2] === "add") {
-
+    let [framework, group, component] = args[3].split("/")
+    if (
+        !framework
+    ) {
+        log("")
+    }
 }
+
+
+
+
+
--- utils.js
+++ utils.js
@@ -1,34 +1,43 @@
 import chalk from "chalk";
 
 export function generateComponentTree(component, indent = 0) {
-  const indentString = '│  '.repeat(indent);
-  const lineCharacter = indent === 0 ? '' : '├─ ';
+    const indentString = '│  '.repeat(indent);
+    const lineCharacter = indent === 0 ? '' : '├─ ';
 
-  console.log(indentString + lineCharacter + chalk.bold(component.name));
+    console.log(indentString + lineCharacter + chalk.bold(component.name));
 
-  if (component.layouts && component.layouts.length > 0) {
-    console.log(indentString + '│  ' + chalk.yellow.bold('Layouts'));
-    component.layouts.forEach((layout, index) => {
-      const isLast = index === component.layouts.length - 1;
-      const layoutCharacter = isLast ? '└─ ' : '├─ ';
-      console.log(indentString + '│  ' + layoutCharacter + chalk.green(layout.name) + ': ' + chalk.gray(layout.description));
-    });
-  }
+    if (component.layouts && component.layouts.length > 0) {
+        console.log(indentString + '│  ' + chalk.yellow.bold('Layouts'));
+        component.layouts.forEach((layout, index) => {
+            const isLast = index === component.layouts.length - 1;
+            const layoutCharacter = isLast ? '└─ ' : '├─ ';
+            console.log(indentString + '│  ' + layoutCharacter + chalk.green(layout.name) + ': ' + chalk.gray(layout.description));
+        });
+    }
 
-  if (component.ui && component.ui.length > 0) {
-    console.log(indentString + '│  ' + chalk.yellow.bold('UI'));
-    component.ui.forEach((uiComponent, index) => {
-      const isLast = index === component.ui.length - 1;
-      const uiCharacter = isLast ? '└─ ' : '├─ ';
-      console.log(indentString + '│  ' + uiCharacter + chalk.blue(uiComponent.name) + ': ' + chalk.gray(uiComponent.description));
-    });
-  }
+    if (component.ui && component.ui.length > 0) {
+        console.log(indentString + '│  ' + chalk.yellow.bold('UI'));
+        component.ui.forEach((uiComponent, index) => {
+            const isLast = index === component.ui.length - 1;
+            const uiCharacter = isLast ? '└─ ' : '├─ ';
+            console.log(indentString + '│  ' + uiCharacter + chalk.blue(uiComponent.name) + ': ' + chalk.gray(uiComponent.description));
+        });
+    }
 
-  if (component.subcomponents && component.subcomponents.length > 0) {
-    component.subcomponents.forEach((subcomponent, index) => {
-      const isLast = index === component.subcomponents.length - 1;
-      const subComponentCharacter = isLast ? '└─ ' : '├─ ';
-      generateComponentTree(subcomponent, indent + 1);
-    });
-  }
+    if (component.subcomponents && component.subcomponents.length > 0) {
+        component.subcomponents.forEach((subcomponent, index) => {
+            const isLast = index === component.subcomponents.length - 1;
+            const subComponentCharacter = isLast ? '└─ ' : '├─ ';
+            generateComponentTree(subcomponent, indent + 1);
+        });
+    }
+}
+
+/**
+    * @param framework {string}
+    * @param frameworks {Record<string, string>}
+    * @returns {boolean}
+    * */
+export function framework_exists(framework, frameworks) {
+    return frameworks.includes(framework)
 }
