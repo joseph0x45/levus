63a2c0c68647811919dfd8e03e2d2f2bb5b0509a
TheWisePigeon <zozozozeph@gmail.com>
2023-06-08 22:20:54 +0000

    updates

---
+++ svelte.js
@@ -0,0 +1,18 @@
+const svelte = {
+    path: "./components/svelte",
+    name:"Svelte",
+    layouts: [
+        {
+            name: "Centered",
+            description: `A container that spans the full screen and centers its children vertically and horizontally.`
+        },
+    ],
+    ui: [
+        {
+            name: "Button",
+            description: "A simple customizable button."
+        }
+    ]
+}
+
+export default svelte
