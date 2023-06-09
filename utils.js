143c6302eb328a5268990435bb795e2d17e51ac8
dRect911 <n3m0ken@gmail.com>
2023-06-09 12:09:58 +0000

    update: update 'generateComponentTree' function. Update function to loop over the component groups dynamically.

--- utils.js
+++ utils.js
@@ -9,52 +9,43 @@ import path from "path";
  */
 
 /**
- * Generates a tree representation of the component and its subcomponents.
- * @param {Object} component - The component object to generate the tree for.
- * @param {number} indent - The current indentation level (default: 0).
+ * Generates a tree-like representation of the components within a framework.
+ *
+ * @param {Object} framework - The framework object containing component details.
+ * @param {number} [indent=0] - The indentation level for the tree structure.
+ * @returns {void}
  */
-export function generateComponentTree(component, indent = 0) {
-  //TODO adapt this function to loop into framework.component_groups array to render each tree list
+export function generateComponentTree(framework, indent = 0) {
   const indentString = "│  ".repeat(indent);
   const lineCharacter = indent === 0 ? "" : "├─ ";
 
-  console.log(indentString + lineCharacter + chalk.bold(component.name));
+  console.log(
+    indentString + lineCharacter + chalk.bold(`${framework.name} components`)
+  );
 
-  if (component.layouts && component.layouts.length > 0) {
-    console.log(indentString + "│  " + chalk.yellow.bold("Layouts"));
-    component.layouts.forEach((layout, index) => {
-      const isLast = index === component.layouts.length - 1;
-      const layoutCharacter = isLast ? "└─ " : "├─ ";
-      console.log(
-        indentString +
-          "│  " +
-          layoutCharacter +
-          chalk.green(layout.name) +
-          ": " +
-          chalk.gray(layout.description)
-      );
+  if (framework.component_groups && framework.component_groups.length > 0) {
+    framework.component_groups.forEach((group) => {
+      console.log(indentString + "│  " + chalk.yellow.bold(group.name));
+
+      group.components.forEach((component, index) => {
+        const isLast = index === group.components.length - 1;
+        const componentCharacter = isLast ? "└─ " : "├─ ";
+
+        console.log(
+          indentString +
+            "│  " +
+            componentCharacter +
+            chalk.cyan(component.name) +
+            ": " +
+            chalk.gray(component.description)
+        );
+      });
     });
   }
 
-  if (component.ui && component.ui.length > 0) {
-    console.log(indentString + "│  " + chalk.yellow.bold("UI"));
-    component.ui.forEach((uiComponent, index) => {
-      const isLast = index === component.ui.length - 1;
-      const uiCharacter = isLast ? "└─ " : "├─ ";
-      console.log(
-        indentString +
-          "│  " +
-          uiCharacter +
-          chalk.blue(uiComponent.name) +
-          ": " +
-          chalk.gray(uiComponent.description)
-      );
-    });
-  }
-
-  if (component.subcomponents && component.subcomponents.length > 0) {
-    component.subcomponents.forEach((subcomponent, index) => {
-      const isLast = index === component.subcomponents.length - 1;
+  if (framework.subcomponents && framework.subcomponents.length > 0) {
+    framework.subcomponents.forEach((subcomponent, index) => {
+      const isLast = index === framework.subcomponents.length - 1;
       const subComponentCharacter = isLast ? "└─ " : "├─ ";
       generateComponentTree(subcomponent, indent + 1);
     });
